534c4a713fdad5452e1ae1b1fded8ab9b32ba697
Vinícius Melo <viniciusdesk@icloud.com>
2018-10-05 12:09:13 -0300

    Adicionada variavel e print

--- test_git.rb
+++ test_git.rb
@@ -2,3 +2,7 @@ puts 'Estou criando um repositorio para Ruby'
 puts 'Praticando minhas skills em Ruby!'
 puts 'Adicionada a terceira linha para o GIT!'
 
+variavel_teste = 25
+
+puts variavel_teste
+
