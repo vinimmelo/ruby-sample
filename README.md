1e045a8a0f1ab53683d281752a947b39ba18ef0a
Vinícius Melo <viniciusdesk@icloud.com>
2018-10-28 00:26:59 -0300

    Modificado maior_menor e README

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Ruby Samples and Training
 Just a training repo, codes made by the lecture of books and online courses. 
 
 Personal projects will come later.
+### References
+
+- Head First Ruby
+- Ruby Under Microscope
+- Ruby On Rails Course (Alura -> https://alura.com.br)
--- maior_menor.rb
+++ maior_menor.rb
@@ -8,8 +8,20 @@ puts "\n\n\n\n\nComeçaremos o jogo para você, " + nome + "."
 puts "Escolhendo um número secreto entre 0 e 200...\n\n\n\n\n\n\n"
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-puts "Tentativa 1"
-puts "Entre com o número: "
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-puts chute.to_i == numero_secreto
+
+for tentativa in 1..3
+  puts "Tentativa " + tentativa.to_s
+  puts "Entre com o número: "
+  chute = gets
+  puts "Será que acertou? Você chutou " + chute.to_s
+  if chute.to_i == numero_secreto
+    puts "Acertou, parabéns!!!"
+    exit
+  else
+    if chute.to_i > numero_secreto
+      puts "O número é menor."
+    else
+      puts "O número é maior."
+    end
+  end
+end
