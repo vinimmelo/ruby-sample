560bc25e97451c74b62c33bfce653d5541c2d021
Vinícius Melo <viniciusdesk@icloud.com>
2018-10-31 13:52:31 -0300

    Refatorado o código, substituido trechos do código por funções; aumentada a legibilidade e manutenabilidade.

--- maior_menor.rb
+++ maior_menor.rb
@@ -1,31 +1,48 @@
-# coding: utf-8
+#!/usr/bin/env ruby
 
+def da_boas_vindas
+  puts 'Bem vindo ao jogo da adivinhação'
+  puts 'Qual é o seu nome? '
+  nome = gets.chomp
+  puts "Começaremos o jogo para você, " + nome + "."
+end
 
-puts 'Bem vindo ao jogo da adivinhação'
-puts 'Qual é o seu nome? '
-nome = gets.chomp
+def sorteia_numero_secreto
+  puts "Escolhendo um número secreto entre 0 e 200...\n\n\n\n\n\n\n"
+  numero_secreto = 175
+  puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+  numero_secreto
+end
 
-puts "\n\n\n\n\nComeçaremos o jogo para você, " + nome + "."
-
-puts "Escolhendo um número secreto entre 0 e 200...\n\n\n\n\n\n\n"
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-
-limite_de_tentativas = 5
-for tentativa in 1..limite_de_tentativas
-  puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+def pede_um_numero(tentativa, limite)
+  puts "\n\n\n\nTentativa " + tentativa.to_s + " de " + limite.to_s
   puts "Entre com o número: "
   chute = gets
-  puts "Será que acertou? Você chutou " + chute.to_s
-  acertou = chute.to_i == numero_secreto
-  if acertou 
+  puts "Será que acertou? Você chutou " + chute
+  chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+  acertou = chute == numero_secreto
+  if acertou
     puts "Acertou, parabéns!!!"
-    break
-  else
-    if chute.to_i > numero_secreto
+    return true
+  end
+    if chute > numero_secreto
       puts "O número é menor."
     else
       puts "O número é maior."
     end
+  false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secretonp
+
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+  chute = pede_um_numero tentativa, limite_de_tentativas
+  if verifica_se_acertou numero_secreto, chute
+    break
   end
 end
