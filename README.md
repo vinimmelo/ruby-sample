849c72e80dc96eda08ae754065d26005002c63b1
Melo <Vinicius.Melo1@br.nestle.com>
2018-10-31 23:16:22 -0300

    Change README and teste.rb

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ruby-sample
 Ruby Samples and Training
 
-Just a training repo, codes made by the lecture of books and online courses. 
+Just a training repository, codes made by the reading of books and online courses.
 
 Personal projects will come later.
 ### References
 
 - Head First Ruby
 - Ruby Under Microscope
-- Ruby On Rails Course (Alura -> https://alura.com.br)
+- Ruby On Rails Course (Alura -> [Alura](https://alura.com.br))
--- teste.rb
+++ teste.rb
@@ -14,13 +14,31 @@ class DigitalCamera < Camera
   end
 end
 
-camera = Camera.new
-camera.load
-camera.take_picture
-
-camera2 = DigitalCamera.new
-camera2.load
-camera2.take_picture
-
-#sub >> 4 | Overriding >> 1 | Inheritance >> 2 
+def teste_camera
+  camera = Camera.new
+  camera.load
+  camera.take_picture
+  camera2 = DigitalCamera.new
+  camera2.load
+  camera2.take_picture
+end
+#sub >> 4 | Overriding >> 1 | Inheritance >> 2
 #superclass >> 3
+
+# test yield use
+def make_casserole
+  ingredients = yield
+  puts "Preheat oven to 375 degrees"
+  puts "Place #{ingredients} in dish"
+  puts "Bake for 20 minutes"
+end
+
+def casserole_use
+  make_casserole {"noodles, celery, and tuna"}
+  make_casserole do
+    "rice, broccoli, and chicken"
+  end
+end
+
+p [1, 2, 3, 4, 5].find_all { |number| number.even? }
+p [1, 2, 3, 4, 5].find_all { |number| number.odd? }
