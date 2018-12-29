b68dc6f06823085777d175638ab218823dcc3e5c
Melo <Vinicius.Melo1@br.nestle.com>
2018-12-28 22:54:25 -0200

    Improved comments modules and added tests.

--- head-first-ruby/clips.rb
+++ head-first-ruby/clips.rb
@@ -1,50 +1,56 @@
-class Clip
+module AcceptsComments
 
-  attr_reader :comments
-
-  def initialize
-    @comments = []
+  def comments
+      @comments ||= []
   end
 
   def add_comment(comment)
     comments << comment
   end
 
+end
+
+
+class Clip
   def play
     puts "Playing #{object_id}..."
   end
-
 end
 
+
 class Video < Clip
+  include AcceptsComments
   attr_accessor :resolution
 end
 
+
 class Song < Clip
+  include AcceptsComments
   attr_accessor :beats_per_minute
 end
 
-module MyModule
-  def first_method
-    puts "first_method called"
-  end
-  def second_method
-    puts "second_method called"
+
+class Photo
+  include AcceptsComments
+  def show
+    puts "Displaying #{object_id}..."
   end
 end
 
-class MyClass
-  include MyModule
-end
 
-my_object = MyClass.new
-my_object.first_method
-my_object.second_method
 
 video = Video.new
 video.add_comment("Cool slow motion effect!")
 video.add_comment("Weird ending.")
 song = Song.new
 song.add_comment("Awesome beat.")
+photo = Photo.new
+photo.add_comment("Nice photo, cool breeze...")
+photo.add_comment("Well done in this picture, cool light.")
 
 p video.comments, song.comments
+p photo.comments
+
+photo.show
+video.play
+song.play
