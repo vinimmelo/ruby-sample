55dbfaa6146db0b6c3c3d5e99bbb540d2cfec78f
Melo <Vinicius.Melo1@br.nestle.com>
2019-01-14 22:51:59 -0200

    Added Comparable module to steak class.

--- steak_mixin.rb
+++ steak_mixin.rb
@@ -1,17 +1,31 @@
 class Steak
   attr_accessor :grade
 
-  def >(other)
-    grade_scores = {"Prime" => 3, "Choice" => 2, "Select" => 1}
-    grade_scores[grade] > grade_scores[other.grade]
+  include Comparable
+
+  GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+
+  def <=>(other)
+    if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+      return -1
+    elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+      return 0
+    else
+      return 1
+    end
   end
 end
 
-first_steak = Steak.new
-first_steak.grade = "Prime"
-second_steak = Steak.new
-second_steak.grade = "Choice"
-
-if first_steak > second_steak
-  puts "I'll take #{first_steak.inspect}."
-end
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+print "choice.between?(select, prime): "
+puts choice.between?(select, prime)
