703b45c07237419eb1bfce93bd260cf83ad56f1b
Melo <Vinicius.Melo1@br.nestle.com>
2019-02-28 00:18:03 -0300

    Addition of word spliiter, mixin with enumerable.

---
+++ head-first-ruby/mixins/word_splitter.rb
@@ -0,0 +1,32 @@
+class WordSplitter
+  attr_accessor :string
+
+  include Enumerable
+
+  def each
+    string.split(" ").each do |word|
+      yield word
+    end
+  end
+
+end
+
+
+splitter = WordSplitter.new
+splitter.string = "one two three four"
+
+splitter.each do |word|
+  puts word
+end
+
+splitter = WordSplitter.new
+splitter.string = "how do you do"
+
+p splitter.find_all { |word| word.include?("d") }
+p splitter.reject { |word| word.include?("d") }
+p splitter.map { |word| word.reverse }
+
+p splitter.any? { |word| word.include?("e") }
+p splitter.count
+p splitter.first
+p splitter.sort
