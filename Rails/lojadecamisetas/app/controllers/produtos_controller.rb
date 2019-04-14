fbe911c2d437e6b91ab61a4b31b42dee85b78558
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-14 14:02:00 -0300

    Now products are ordered by name and price accordly.

--- Rails/lojadecamisetas/app/controllers/produtos_controller.rb
+++ Rails/lojadecamisetas/app/controllers/produtos_controller.rb
@@ -1,2 +1,7 @@
 class ProdutosController < ApplicationController
+
+    def index
+        @produtos_por_nome = Produto.order(:nome).limit 5
+        @produtos_por_preco = Produto.order(:preco).limit 2
+    end
 end
