a82f940dd410e70e4d4bd61c885b60d0b7945ee0
Vinicius Melo <viniciusdesk@icloud.com>
2019-06-23 11:21:58 -0300

    Addition of method to create a Product based in the product model

--- Rails/lojadecamisetas/app/controllers/produtos_controller.rb
+++ Rails/lojadecamisetas/app/controllers/produtos_controller.rb
@@ -4,4 +4,10 @@ class ProdutosController < ApplicationController
         @produtos_por_nome = Produto.order(:nome).limit 5
         @produtos_por_preco = Produto.order(:preco).limit 2
     end
+
+    def create
+      valores = params.require(:produto).permit :nome, :preco, :quantidade, :descricao
+      produto = Produto.create valores
+    end
+
 end
