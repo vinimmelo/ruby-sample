b49fc1e51da6e22b4a7223bc3ecca0122e8e5f32
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-08-19 18:56:41 -0300

    Add _why poignant folder and scripts

---
+++ WhysPoignant/wordlist.rb
@@ -0,0 +1,6 @@
+CODE_WORDS = {
+  'starmonkeys' => 'Phil and Pete, those prickly chancellors of the New Reich',
+  'catapult' => 'chucky go-go', 'firebomb' => 'Heat-Assisted Living',
+  'Nigeria' => "Ny and Jerry's Dry Cleaning (with Donuts)",
+  'Put the kibosh on' => 'Put the cable box on'
+}
