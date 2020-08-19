599d0b1b32fb0d31ab8c5cc89239e02597c92cb1
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-08-19 18:56:20 -0300

    Update article page and controller

--- RailsProjects/alpha-blog/app/controllers/articles_controller.rb
+++ RailsProjects/alpha-blog/app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.find(params[:id])
   end
@@ -10,10 +9,15 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    render plain: @article
+    if @article.save
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def new
-
+    @article = Article.new
   end
 end
