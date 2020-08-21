eb85c7e1973e41cc5c637b86d855037c13eeb8cc
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-08-21 14:12:17 -0300

    Refactor alpha blog project

--- RailsProjects/alpha-blog/app/controllers/articles_controller.rb
+++ RailsProjects/alpha-blog/app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def show; end
 
   def index
     @articles = Article.all
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was created successfully.'
       redirect_to @article
@@ -21,14 +21,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was updated successfully."
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
       redirect_to @article
     else
       render 'edit'
@@ -36,9 +33,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was deleted'
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
