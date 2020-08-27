87a47b1be140a6b08be20cbffdfda0f0ef3e096d
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-08-27 13:46:58 -0300

    Update articles form and views

--- RailsProjects/alpha-blog/app/models/user.rb
+++ RailsProjects/alpha-blog/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_many :articles
   validates :username, presence: true, length: { minimum: 3, maximum: 25 },
                        uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 105 },
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
